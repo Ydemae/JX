a0023e2d23c3c16c151743df6f2c24cdf9b5b4e6
Ydemae <113693952+Ydemae@users.noreply.github.com>
2022-11-16 15:52:40 +0100

    Update arthur.c

--- arthur.c
+++ arthur.c
@@ -3,7 +3,7 @@
 void afficheRegles()
 {   
     char veutAfficheRegles;
-    printf("Bienvenue,\nvous etes en train de jouer au JeuX edtition AT mais peut etre que c'est votre premiere fois, souhaitez vous voir les regles du jeu? (repondre O pour oui N pour non)");
+    printf("Bienvenue,\nvous etes en train de jouer au JeuX edition AT mais peut etre que c'est votre premiere fois, souhaitez vous voir les regles du jeu? (repondre O pour oui N pour non)");
     scanf("%c", &veutAfficheRegles);
     if(veutAfficheRegles == 'O' || veutAfficheRegles == 'o') printf("Ce jeu se joue a deux et se deroule sur un plateau defini par un nombre de ligne et de colonnes impair comme celui ci :\n\e[0;32m0    1   2   3\e[0m\n\e[0;32m1\e[0m||  0|   0|   0|\n\e[0;32m2\e[0m||  0|   0|   0|\n\e[0;32m3\e[0m||  0|   0|   0|\nVous devrez placer des jetons numerotes en fonction du tour actuel, partant donc de 1.\nVous devrez evidemment faire attention a cibler une case qui figure dans le tableau et a ne pas tenter de poser de jeton sur un jeton deja en jeu, ceci etant contraire aux regles.\nCes erreurs vous renverraient ce message d'erreur \e[7;31mveuillez saisir une case valide\e[0m .\nLe gagnant du jeu est determine en fonction du dernier 0 en jeu. En effet, toutes les cases autour de ce 0 seront comptabilisees dans le score d'un des deux joueurs, c'est ce qui donne une dimension plus strategique a ce jeu, vous permettant de retirer des conditions de victoire a votre adversaire. Nous esperons que cette breve explication vous aura ete utile mais rien n'est mieux qu'une partie pour comprendre le fonctionnement du jeu, nous vous souhaitons donc bon jeu, \e[1;33mHave Fun !\e[0m\n");
 }
